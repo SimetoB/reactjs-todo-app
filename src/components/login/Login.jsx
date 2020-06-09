2225b0e673d80779f2a66b205cb859d0cc7d2c2a
Simeon Bakov <cocakoca@gmail.com>
2020-06-10 00:02:43 +0300

    Created login component

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+class Login extends Component{
+    handleSubmit(event) {
+        const user = document.getElementById("user").value;
+        const password = document.getElementById("password").value;
+        var credential = {[user] : password};
+        console.log(credential);
+
+        if (localStorage.getItem(user) === password) {
+            alert("Login successful!");
+        } else {
+            alert("Invalid login! Please try again.");
+        }
+
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div class="wrapper">
+                <form method="post" class="login-form" onSubmit={this.handleSubmit}>
+                    <p class="label">Username:</p>
+                    <input type="text" name="user" id="user" class="input" />
+                    <p class="label">Password:</p>
+                    <input type="password" name="password" id="password" class="input" /><br />
+                    <p>
+                        <button type="submit" class="submit" id="login">Login</button>
+                        <Link to="/register" class="register">Register here</Link>
+                    </p>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
