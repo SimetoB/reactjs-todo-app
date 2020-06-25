e5f184cda278605c1820cc261533e33caed0f0a2
Simeon Bakov <cocakoca@gmail.com>
2020-06-25 22:14:33 +0300

    Added user auth on component mount and proper user display

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,6 +3,29 @@ import './Main.css';
 
 class Main extends Component{
 
+    componentDidMount (){
+        var loggedUser = localStorage.getItem("loggedUser");
+        if (loggedUser === null) {
+            alert("You're not logged in, you will be redirect to the login page.")
+            window.location.href="/login";
+        } else {
+            document.getElementById("user").innerHTML = loggedUser;
+        }
+    }
+
+    dropdownClicked(){
+        if ( document.getElementById("signout").style.display === "") {
+            document.getElementById("signout").style.display = "block";
+        } else {
+            document.getElementById("signout").style.display = "";
+        }
+    }
+
+    signOut(){
+        localStorage.removeItem("loggedUser");
+        alert("Signout successful.");
+        window.location.href="/login";
+    }
 
     render() {
         return (
@@ -10,13 +33,16 @@ class Main extends Component{
                 <header>
                     <p>
                         <h1 class="title">Todo app</h1>
-                        <span class="user-placeholder">Username placeholder</span>
+                        <span class="user-placeholder">
+                            <span class="user" id="user" onClick={this.dropdownClicked}></span>
+                            <span class="signout" id="signout" onClick={this.signOut}>Signout</span>
+                        </span>
                     </p>
                 </header>
                 <main class="todos-wrapper">
                     <p class="sub-title">My todos:</p>
 
-                    <ul class="todos">
+                    <ul class="todos" id="todos">
                         <li class="todo">Testing a todo appearence</li>
                         <li class="todo">Testing a todo appearence</li>
                         <li class="todo">Testing a todo appearence</li>
